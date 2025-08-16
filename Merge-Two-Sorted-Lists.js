81af9b39e299cb2ee17f1af02daee1c4255399f8
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2025-08-16 17:31:30 +0500

    [21] [Time Beats: 100.00%] [Memory Beats: 6.62%] - LeetPush

---
+++ Merge-Two-Sorted-Lists.js
@@ -0,0 +1,13 @@
+function mergeTwoLists(list1, list2) {
+    let l1 = list1; let l2 = list2;
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+}
