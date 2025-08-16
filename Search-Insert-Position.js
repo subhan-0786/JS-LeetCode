57e903c2af1195344d5270e0a81d7222f727d38f
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2025-08-16 17:34:43 +0500

    [35] [Time Beats: 100.00%] [Memory Beats: 71.98%] - LeetPush

---
+++ Search-Insert-Position.js
@@ -0,0 +1,10 @@
+function searchInsert(nums, target) {
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = (left + right) >> 1;
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return left;
+}
