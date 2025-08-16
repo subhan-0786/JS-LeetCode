2b771011f0303e4488478691743759445678fcc2
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2025-08-16 17:36:11 +0500

    [18] [Time Beats: 91.29%] [Memory Beats: 57.90%] - LeetPush

---
+++ 4Sum.js
@@ -0,0 +1,40 @@
+var fourSum = function(nums, target) {
+  const result = [];
+
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 3; i++) {
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      let high = nums.length - 1;
+      let low = j + 1;
+
+      while (low < high) {
+        const sum = nums[i] + nums[j] + nums[low] + nums[high];
+
+        if (sum === target) {
+          result.push([nums[i], nums[j], nums[low], nums[high]]);
+          while (nums[low] === nums[low + 1]) {
+            low++;
+          }
+          while (nums[high] === nums[high - 1]) {
+            high--;
+          }
+          low++;
+          high--;
+        } else if (sum < target) {
+          low++;
+        } else {
+          high--;
+        }
+      }
+      while (nums[j] === nums[j + 1]) {
+        j++;
+      }
+    }
+    while (nums[i] === nums[i + 1]) {
+      i++;
+    }
+  }
+
+  return result;
+};
