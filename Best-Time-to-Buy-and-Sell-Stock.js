a2236e9f9888c06b86faf47baf64dbe77cf73de8
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2025-08-19 13:16:46 +0500

    [121] [Time Beats: 10.94%] [Memory Beats: 25.13%] - LeetPush

---
+++ Best-Time-to-Buy-and-Sell-Stock.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0], maxProfit = 0;
+    for (let price of prices) {
+        if (price < minPrice) minPrice = price;
+        else maxProfit = Math.max(maxProfit, price - minPrice);
+    }
+    return maxProfit;
+};
