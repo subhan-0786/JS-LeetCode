a44d7dff7f2fd6a7a90d373b903621f4c2e56d34
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2025-08-26 02:47:22 +0500

    [2665] [Time Beats: 5.20%] [Memory Beats: 55.18%] - LeetPush

---
+++ Counter-II.js
@@ -0,0 +1,22 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+
+    return {
+        increment: function() {
+            current += 1;
+            return current;
+        },
+        decrement: function() {
+            current -= 1;
+            return current;
+        },
+        reset: function() {
+            current = init;
+            return current;
+        }
+    };
+};
