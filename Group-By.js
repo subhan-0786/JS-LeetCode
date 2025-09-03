3397cc260ab44efd1411b6876e6e39bb377cfe51
Subhan Amjad <124813748+subhan-0786@users.noreply.github.com>
2025-09-03 23:35:04 +0500

    [2631] [Time Beats: 93.75%] [Memory Beats: 64.52%] - LeetPush

---
+++ Group-By.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, item) => {
+        const key = fn(item);
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(item);
+        return acc;
+    }, {});
+};
